9698e68853e68c56c4ceb7c7b200e5207a9eb5b3
Bhupendr1 <bhupendranirwan737@gmail.com>
2023-06-30 13:16:33 +0530

    june30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'multilevelReactiveForm';
+  recipeForm!: FormGroup;
+  years: number[];
+
+  constructor(private fb: FormBuilder) {
+    this.years = this.getYears();
+    this.recipeForm = this.fb.group({
+      personalInfo: this.fb.group({
+        firstname: ['', Validators.required],
+        Lastname: ['', Validators.required],
+        Nickname: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        year: ['', Validators.required],
+        Passport: ['', Validators.required]
+      }),
+
+      address: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        zipCode: ['', [Validators.required, Validators.pattern('[0-9]{5}')]],
+      }),
+      reciepe: this.fb.array([
+        this.createItem()
+      ]),
+      skills: this.fb.record<boolean>({})
+    });
+  }
+
+  private getYears(): number[] {
+    const now = new Date().getUTCFullYear();
+    return Array(now - (now - 40)).fill('').map((_, idx) => now - idx)
+  }
+  // private buildSkillControls(skills:string){
+  //   skills.forEach((skill: any) =>
+  //      this.recipeForm.addControl(
+  //     skill,new FormControl(false,{nonNullable:true})
+  //   ))
+  // }
+  createItem(): FormGroup {
+    return this.fb.group({
+      PhoneSelect: ['', Validators.required],
+      phoneNumber: ['', Validators.required]
+    });
+  }
+  get itemControls() {
+    return this.recipeForm.get('reciepe') as FormArray;
+  }
+
+  addItem(): void {
+    this.itemControls.push(this.createItem());
+  }
+
+  submitForm() {
+    console.log(this.recipeForm.value);
+  }
+
+  removeItem(index: number): void {
+    this.itemControls.removeAt(index);
+  }
 }
---
+++ src/app/user-info.ts
@@ -0,0 +1,11 @@
+export interface UserInfo{
+    firstName:string;
+    lastName:string;
+    nickname:string;
+    email:string;
+    yearOfBirth:string;
+    passport:string;
+    fullAdress:string;
+    city:string;
+    postCode:number;
+}
